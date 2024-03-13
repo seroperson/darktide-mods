94ab5366a942f49fd5a57f0ad46ea7bce0138158
deluxghost <deluxghost@users.noreply.github.com>
2024-03-13 10:40:34 +0800

    Update FilterTrash_localization.lua

--- FilterTrash/scripts/mods/FilterTrash/FilterTrash_localization.lua
+++ FilterTrash/scripts/mods/FilterTrash/FilterTrash_localization.lua
@@ -2,29 +2,36 @@ return {
 	mod_name = {
 		en = "Filter Trash",
 		ru = "Фильтр мусора в магазине",
+		["zh-cn"] = "屏蔽垃圾",
 	},
 	mod_description = {
 		en = "Filters Brunt's store weapon items by predefined rules.",
 		ru = "Фильтрует оружие в магазине по заданным правилам.",
+		["zh-cn"] = "根据预先配置的规则屏蔽布伦特商店内的垃圾物品。",
 	},
 	group_filter_by_base_item_level = {
 		en = "Filter by base item level",
 		ru = "Фильтр по базовому уровню",
+		["zh-cn"] = "根据物品基础评级过滤",
 	},
 	ignore_filtering_when_unlearned = {
 		en = "Ignore filtering when item has unlearned perk",
 		ru = "Игнорировать фильтры при неизученном таланте",
+		["zh-cn"] = "物品有尚未提取的祝福则不过滤",
 	},
 	group_filter_by_stats = {
 		en = "Filter by stats",
 		ru = "Фильтр по характеристикам",
+		["zh-cn"] = "根据五维过滤",
 	},
 	item_level_base = {
 		en = "Value",
 		ru = "Значение",
+		["zh-cn"] = "值",
 	},
 	stat_level = {
 		en = "Value (%%)",
 		ru = "Значение (%%)",
+		["zh-cn"] = "值（%%）",
 	}
 }
